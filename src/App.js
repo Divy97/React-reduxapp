6ecdc875aa21bf963b7a5e5288502ec6ad4628e9
Divy Parekh <divyparekh1810@gmail.com>
2022-04-27 18:21:52 +0530

    Redux Provider is Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Todo from "./components/Todo"
+import TodoForm from './components/TodoForm';
 
+import {Provider} from "react-redux"
+// import from ""
 function App() {
   return (
-    <div className="App"> 
-        <h1>It workss</h1>
-    </div>
+    <Provider>
+      <Container>
+        <Todo />
+        <TodoForm />
+      </Container>
+    </Provider>
   );
 }
 
